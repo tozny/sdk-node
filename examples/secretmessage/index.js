508fabbcaf14cecc331617240eab609015d55c19
Jesse Hallett <jesse@galois.com>
2014-10-30 21:52:44 -0700

    Lets jquery.tozny.js send login_challenge; uses express-session

--- examples/secretmessage/index.js
+++ examples/secretmessage/index.js
@@ -1,13 +1,22 @@
 var express     = require('express')
+  , session     = require('express-session')
   , Realm       = require('tozny-sdk/src/realm')
-  , User        = require('tozny-sdk/src/user')
   , bodyParser  = require('body-parser')
-  , cookie      = require('cookie')
 ;
 
 var app = express();
 app.set('views', './views');
 app.set('view engine', 'jade');
+
+app.use(session({
+  secret: 'app secret',
+  cookie: {
+    httpOnly: true,
+    secure:   process.env.NODE_ENV === 'production'
+  },
+  resave: true,
+  saveUninitialized: false
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var realm = new Realm(
@@ -15,31 +24,22 @@ var realm = new Realm(
   process.env.npm_package_config_realm_secret,
   process.env.npm_package_config_api_url
 );
-var user = new User(realm.keyId, realm.apiUrl);
-
-var authenticated = {};
 
 app.get('/', function(req, res) {
-  var sessionId = getcookie(req, 'sid');
-  if (authenticated[sessionId]) {
+  if (req.session.authenticated) {
     res.redirect('/secret');
   }
   else {
-    user.loginChallenge().then(function(resp) {
-      res.render('index', {
-        realm:      realm,
-        session_id: resp.session_id,
-        qr_url:     resp.qr_url,
-        mobile_url: resp.mobile_url
-      });
-    });
+    res.render('index', { realm: realm });
   }
 });
 
 app.get('/secret', function(req, res) {
-  var sessionId = getcookie(req, 'sid');
-  if (authenticated[sessionId]) {
-    res.render('secret', { message: process.env.npm_package_config_message });
+  if (req.session.authenticated) {
+    res.render('secret', {
+      message: process.env.npm_package_config_message,
+      user:    req.session.user
+    });
   }
   else {
     res.redirect('/');
@@ -50,11 +50,14 @@ app.post('/login', function(req, res) {
   var signedData = req.body.tozny_signed_data;
   var signature  = req.body.tozny_signature;
   realm.verifyLogin(signedData, signature).then(
-    function success(resp) {
-      authenticated[resp.session_id] = true;
-      setcookie(res, 'sid', resp.session_id);
-      res.redirect('/secret');
-    },
+    function success(login) {
+      return realm.userGet(login.user_id).then(function(user) {
+        req.session.authenticated = true;
+        req.session.user = user;
+        res.redirect('/secret');
+      });
+    }
+  ).then(null,
     function error(err) {
       res.render('error', { message: err });
     }
@@ -62,31 +65,10 @@ app.post('/login', function(req, res) {
 });
 
 app.post('/logout', function(req, res) {
-  var sessionId = getcookie(req, 'sid');
-  delete authenticated[sessionId];
-  res.redirect('/');
-});
-
-function getcookie(req, name) {
-  var header = req.headers.cookie;
-  if (header) {
-    var cookies = cookie.parse(header);
-    return cookies[name];
-  }
-}
-
-function setcookie(res, name, val) {
-  var data = cookie.serialize(name, val, {
-    path:     '/',
-    httpOnly: true,
-    secure:   process.env.NODE_ENV === 'production'
+  req.session.destroy(function() {
+    res.redirect('/');
   });
-  var prev = res.get('set-cookie') || [];
-  var header = Array.isArray(prev) ? prev.concat(data)
-                                   : Array.isArray(data) ? [prev].concat(data)
-                                                         : [prev, data];
-  res.set('set-cookie', header);
-}
+});
 
 var server = app.listen(process.env.npm_package_config_port || 3000, function() {
   var host = server.address().address;
